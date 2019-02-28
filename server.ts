5e24dceeacbada29d05169f047ccdaa25e056169
mzuccaroli <marcozuccaroli@gmail.com>
2019-02-28 14:36:36 +0100

    add multilanguage support add language injection by geolocation service add i18n and xliff translation generation

--- server.ts
+++ server.ts
@@ -1,13 +1,13 @@
 import 'zone.js/dist/zone-node';
-import { enableProdMode } from '@angular/core';
-
+import 'reflect-metadata';
+import {enableProdMode} from '@angular/core';
 // Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
+import {ngExpressEngine} from '@nguniversal/express-engine';
 // Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
 
 import * as express from 'express';
-import { join } from 'path';
+import {join} from 'path';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -19,21 +19,18 @@ const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./server/main');
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
   bootstrap: AppServerModuleNgFactory,
   providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
+    provideModuleMap(LAZY_MODULE_MAP),
   ]
 }));
 
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
-
-// Example Express Rest API endpoints
-// app.get('/api/**', (req, res) => { });
 
 // Server static files from /browser
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
@@ -42,7 +39,27 @@ app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
 
 // All regular routes use the Universal engine
 app.get('*', (req, res) => {
-  res.render('index', { req });
+  // this is for i18n
+  const supportedLocales = ['it', 'en', 'es', 'fr'];
+  const defaultLocale = 'en';
+  const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
+
+  // check if the requested url has a correct format '/locale' and matches any of the supportedLocales
+  const locale = (matches && supportedLocales.indexOf(matches[1]) !== -1) ? matches[1] : defaultLocale;
+
+
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  if (ip.substr(0, 7) === '::ffff:') {
+    ip = ip.substr(7);
+  }
+
+  res.render(`${locale}/index`, {
+    req,
+    providers: [
+      {provide: 'language', useFactory: () => locale, deps: []},
+      {provide: 'ip', useFactory: () => ip, deps: []}
+    ]
+  });
 });
 
 // Start up the Node server
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GeolocationService} from '../geolocation.service';
 
 @Component({
-  selector: 'app-home',
-  template: `
-    <h3>{{ message }}</h3>
-    <img [src]="image">
-  `
+    selector: 'app-home',
+    template: `
+        <img [src]="image">
+        <h3 i18n>Hello i18n!</h3>
+        <b>userLanguage:</b> {{userLanguage}}<br/>
+        <b>userCountry:</b> {{userCountry}}<br/>
+        <b>currency:</b>  {{number | currency:'EUR': 'symbol' }}<br/>
+        <b>Today is</b> {{today | date}}<br/>
+        <b>Or if you prefer,</b> {{today | date:'fullDate'}}<br/>
+        <b>The time is</b> {{today | date:'h:mm a z'}}<br/>
+    `
 })
-export class HomeComponent {
-  public message = `Angular Universal`;
-  // tslint:disable-next-line:max-line-length
-  public image = 'https://camo.githubusercontent.com/81f72f2fdf98aa1d30b5b215bc8ca9420b249e81/68747470733a2f2f616e67756c61722e696f2f67656e6572617465642f696d616765732f6d61726b6574696e672f636f6e636570742d69636f6e732f756e6976657273616c2e706e67';
+export class HomeComponent implements OnInit {
+    // tslint:disable-next-line:max-line-length
+    public image = 'https://camo.githubusercontent.com/81f72f2fdf98aa1d30b5b215bc8ca9420b249e81/68747470733a2f2f616e67756c61722e696f2f67656e6572617465642f696d616765732f6d61726b6574696e672f636f6e636570742d69636f6e732f756e6976657273616c2e706e67';
+    public userLanguage: string;
+    public userCountry: string;
+    public number = 10000;
+    public today = Date.now();
 
-  constructor() {}
+
+    constructor(
+        private geolocation: GeolocationService,
+    ) {
+    }
+
+    ngOnInit() {
+        this.geolocation.getClientLocale().subscribe((data) => {
+            this.userLanguage = data.language;
+            this.userCountry = data.country;
+        });
+    }
 }
